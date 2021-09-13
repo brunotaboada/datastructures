5b05611f5fe5598a698fd10f22ea04e3a095bdc4
Bruno Taboada <bruno.taboada@bnc.ca>
2021-09-12 21:37:18 -0400

    demo

---
+++ src/main/java/demo/DemoApplication.java
@@ -0,0 +1,52 @@
+package demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+  public static void main(String[] args) {
+    //SpringApplication.run(DemoApplication.class, args);
+    String word = "abcdefghijklmno";
+    StringBuilder[] result = parts(word, 2);
+
+    int bucketIndex = 0;
+    int [] counters = new int[result.length];
+    int idx = 0;
+    int lineControl = 0;
+    int lineSize = result[idx].length();
+    for (int i = 0; i < word.length(); i++) {
+      if (lineControl == lineSize) {
+        System.out.println();
+        lineSize = result[++idx].length();
+        lineControl = 0;
+      }
+      if(bucketIndex == result.length){
+        bucketIndex = 0;
+      }
+      System.out.print(result[bucketIndex].charAt(counters[bucketIndex]++));
+      bucketIndex++;
+      lineControl++;
+    }
+    System.out.println();
+  }
+
+  private static StringBuilder[] parts(String word, int split) {
+    int bucketIndex = 0;
+    StringBuilder[] bucket = new StringBuilder[split];
+    for (int i = 0; i < word.length(); i++) {
+      char letter = word.charAt(i);
+      if(bucketIndex == bucket.length){
+        bucketIndex = 0;
+      }
+      if(bucket[bucketIndex] == null){
+        bucket[bucketIndex] = new StringBuilder();
+      }
+      bucket[bucketIndex].append(letter);
+      bucketIndex++;
+    }
+
+    return bucket;
+  }
+
+}
---
+++ src/main/java/demo/Main.java
@@ -0,0 +1,63 @@
+package demo;
+
+
+import java.util.*;
+
+public class Main {
+
+  public static Integer arrayChallenge(int[] arr) {
+    Map<Integer, Integer> counts = new HashMap<>();
+    Stack<Integer> stack = new Stack<>();
+
+    int high = 0;
+    int higher = 0;
+
+    for(int i = 0; i < arr.length; i++){
+      Integer val = counts.get(arr[i]);
+      if(val != null){
+        val++;
+        counts.put(arr[i], val);
+        stack.add(arr[i]);
+        if(high < val){
+          high = val;
+          higher = arr[i];
+        }
+      } else {
+        counts.put(arr[i], 1);
+      }
+    }
+    if(stack.isEmpty()){
+      return -1;
+    } else {
+      return stack.firstElement() == higher ? stack.firstElement() : higher;
+    }
+  }
+
+  public static boolean StringChallenge(String str1, String str2) {
+    Set<Character> set = new HashSet<>();
+    for(int i = 0; i < str1.length(); i++){
+      set.add(str1.charAt(i));
+    }
+
+    for(int k = 0; k < str2.length(); k++ ){
+      if(!set.contains(str2.charAt(k))){
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  public static void main (String[] args) {
+    // keep this function call here
+    //Scanner s = new Scanner(System.in);
+    //String str1 = s.nextLine();
+    //String str2 = s.nextLine();
+    //System.out.print(StringChallenge(str1, str2));
+
+    System.out.println(arrayChallenge(new int[] {5, 5, 1, 5, 1, 11, 11, 11}));
+
+  }
+
+
+}
