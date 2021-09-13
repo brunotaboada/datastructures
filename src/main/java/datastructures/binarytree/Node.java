82577e0fcb253051cbf6cd00d036e22d074bbecf
Bruno Taboada <bruno.taboada@bnc.ca>
2021-09-12 21:40:04 -0400

    adjustment

--- src/main/java/datastructures/binarytree/Node.java
+++ src/main/java/datastructures/binarytree/Node.java
@@ -1,33 +1,10 @@
 package datastructures.binarytree;
 
-/**
- * Represents a node in the Binary Search Tree.
- * 
- * @param <T>
- */
 public class Node<T> {
-    // The value present in the node.
-    /**
-    * 
-    */
     public int value;
-
-    // The reference to the left subtree.
-    /**
-     * 
-     */
-
     public Node<T> left;
-
-    // The reference to the right subtree.
-    /**
-     * 
-     */
     public Node<T> right;
 
-    /**
-     * @param value
-     */
     public Node(int value) {
         this.value = value;
     }
