fec306a974b87d86c1ffd36c26c13767d13f0a84
brunotaboada <bruno.taboada@mavtek.com>
2024-03-12 18:00:08 -0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/datastructures/stack/MyStackArray.java
+++ src/main/java/datastructures/stack/MyStackArray.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class MyStackArray<E> {
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
-    private Object elements[];
+    private Object[] elements;
 
     public MyStackArray() {
         elements = new Object[DEFAULT_CAPACITY];
