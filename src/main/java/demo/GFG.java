6fdf59366b6ca3679f99b65a28872228efaeac84
brunotaboada <bruno.taboada@mavtek.com>
2025-07-27 21:55:04 -0400

    Enhancement

--- src/main/java/demo/GFG.java
+++ src/main/java/demo/GFG.java
@@ -4,26 +4,23 @@ import java.util.*;
 
 class GFG {
 
-    static int getPairsCount(int arr[], int n, int k) {
+    static int getPairsCount(int[] arr, int k) {
         HashMap<Integer, Integer> m = new HashMap<>();
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (m.containsKey(k - arr[i])) {
-                count += m.get(k - arr[i]);
-            }
-            if (m.containsKey(arr[i])) {
-                m.put(arr[i], m.get(arr[i]) + 1);
-            } else {
-                m.put(arr[i], 1);
-            }
+        for (int ele : arr) {
+            int targetElement = k - ele;
+            if (m.containsKey(targetElement))
+                count += m.get(targetElement);
+            m.computeIfPresent(ele, (key, value) -> value + 1);
+            m.computeIfAbsent(ele, integer -> 1);
         }
         return count;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 5, 7, -1, 5};
+        int[] arr = {1, 5, 7, -1, 5};
         int sum = 6;
-        System.out.print("Count of pairs is " + getPairsCount(arr, arr.length, sum));
+        System.out.print("Count of pairs is " + getPairsCount(arr, sum));
     }
 }
 
